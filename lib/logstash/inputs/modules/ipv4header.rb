dc9abc1a1e2041f13513160ce2a3d42ce947fa09
Baffin Bay Networks <devops-github@baffinbaynetworks.com>
2015-03-29 21:33:07 +0200

    Update ipv4header.rb

--- lib/logstash/inputs/modules/ipv4header.rb
+++ lib/logstash/inputs/modules/ipv4header.rb
@@ -1,3 +1,14 @@
+##########################################################################################
+# Copyright (C) Buffin Bay Networks, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Devops <devops-github@baffinbaynetworks.com>, March 2015
+##########################################################################################
+# FILE DESCRIPTOR:
+# This plugin is written by Baffin Bay Networks and are being used for
+# receiving and parsing sflow data.
+##########################################################################################
+
 require_relative 'protocol'
 
 class IPv4Header
