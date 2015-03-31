5e62f690188a09aa5328554c2c120266240c782a
Baffin Bay Networks <devops-github@baffinbaynetworks.com>
2015-03-31 21:01:56 +0200

    Update protocol.rb

--- lib/logstash/inputs/modules/protocol.rb
+++ lib/logstash/inputs/modules/protocol.rb
@@ -1,14 +1,4 @@
-##########################################################################################
-# Copyright (C) 2015 Buffin Bay Networks, Inc - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Devops <devops-github@baffinbaynetworks.com>, March 2015
-##########################################################################################
-# FILE DESCRIPTOR:
-# This plugin is written by Baffin Bay Networks and are being used for
-# receiving and parsing sflow data.
-##########################################################################################
-
+# encoding: utf-8
 require_relative 'ipv4header'
 require_relative 'udpheader'
 require_relative 'tcpheader'
